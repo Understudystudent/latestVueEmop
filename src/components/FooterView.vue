6c59c990ef1d09005e82f14c3493ee8035dd8106
Understudystudent <carlmjames2@gmail.com>
2024-01-10 17:34:29 +0200

    added the json data

--- src/components/FooterView.vue
+++ src/components/FooterView.vue
@@ -1,26 +1,28 @@
 <template>
-  <div class="container">
-      <div class="row">
-          <p class="lead">Carl portfolio &copy;
-              <span>  {{ currYear }}</span>
-          </p>
-      </div>
-  </div>
-</template>
-
-<script>
-
+    <div class="container">
+      <a href="https://github.com/Understudystudent" target="_blank">
+        <i class="bi bi-github" style="font-size: 2rem; margin-right: 1rem;"></i>
+      </a>
+      <a href="https://www.linkedin.com/in/carl-james-3ab3ba276/" target="_blank">
+        <i class="bi bi-linkedin" style="font-size: 2rem; margin-right: 1rem;"></i>
+      </a>
+      <p class="lead" style="font-size: 0.8rem;">
+        &copy; <span> {{ currYear }}</span>
+        Carl James. All rights reserved.
+      </p>
+    </div>
+  </template>
+  
+  <script>
   export default {
-      name: 'FooterComp',
-      data(){
-          return{
-              currYear: new Date ().getFullYear()
-          }
-      }
-      
-  }
-</script>
-
-<style scoped>
-
-</style>
+    name: 'FooterComp',
+    data() {
+      return {
+        currYear: new Date().getFullYear()
+      };
+    }
+  };
+  </script>
+  
+  <style scoped></style>
+  
--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,45 @@
 import { createStore } from 'vuex'
 
+const dataUrl = "https://understudystudent.github.io/vueEOMPdata/data/"
+
 export default createStore({
   state: {
+    Jobtitle : null,
+    about: null,
+    education: null,
+    skills: null,
+    testimonials: null,
+    projects: null,
   },
   getters: {
   },
   mutations: {
+    SetJobtitle (state,value){
+      state.Jobtitle = value
+    },
+    SetAbout (state,value){
+      state.about = value
+    },
+    SetEducation (state,value){
+      state.education = value
+    },
+    SetSkills (state,value){
+      state.skills = value
+    },
+    SetTestimonials (state,value){
+      state.testimonials = value
+    },
+    SetProjects (state,value){
+      state.projects = value
+    },
+    
   },
   actions: {
+    async fetchJobTitle(context){
+      let res = await fetch(dataUrl)
+      let {Jobtitle} =  res.json()
+      context.commit('setJobTitle',Jobtitle)
+    }
   },
   modules: {
   }
